db8743365386ca068691cceef8ea841af2413975
sshihata <samy.shihata@yahoo.com>
2014-11-03 19:21:24 +0200

    nn working with stoch grad 100% recog. rate

--- src/main.m
+++ src/main.m
@@ -1,14 +1,14 @@
 clear;
 clc;
 load('../data/xordata.mat');
-% rng('shuffle')
+rng('shuffle')
 
 % important data
 m = size(X,1);
 n = size(X,2);
 
 input_layer_size = n;
-hidden_layer_size = 10;
+hidden_layer_size = 2;
 num_labels = size(y, 2);
 
 % Randomizing data 
@@ -23,25 +23,9 @@ initial_omega = randomInitializeWeights(0, hidden_layer_size - 1, e);
 
 initial_nn_params = [initial_theta1(:); initial_theta2(:); initial_omega(:)];
 
-J = nnCostFunction(initial_nn_params, input_layer_size, hidden_layer_size, ...
-                   num_labels, X, y);
-
-options = optimset('MaxIter', 200);
-
-costFunction = @(p) nnCostFunction(p, ...
-                                   input_layer_size, ...
-                                   hidden_layer_size, ...
-                                   num_labels, X, y, 0);
-
-[nn_params, cost] = nnCostFunction(initial_nn_params, input_layer_size, hidden_layer_size, ...
-                                   num_labels, X, y, 0.001);
-cost
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-	                 hidden_layer_size, (input_layer_size + 1));
-
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-	                 num_labels, (hidden_layer_size + 1));
-
+[Theta1, Theta2, cost] = stoch_grad(initial_nn_params, input_layer_size, hidden_layer_size, ...
+                                   num_labels, X, y, 10^-4);
+                               
 p = predict(Theta1, Theta2, X);
 sum(p ~= y)
 % p~=y 
