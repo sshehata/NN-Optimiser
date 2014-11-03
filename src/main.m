3d93f769f903f26132caa2bbbd9a9a605d09b838
Samy Shihata <samy.s.shihata@gmail.com>
2014-11-03 02:06:48 +0200

    Adding project structure

--- src/main.m
+++ src/main.m
@@ -1,25 +1,27 @@
 clear;
 clc;
-load('xordata.mat');
+load('../data/xordata.mat');
 % rng('shuffle')
 
 % important data
 m = size(X,1);
 n = size(X,2);
 
-input_layer_size = 2;
-hidden_layer_size = 10;
-num_labels = 1;
+input_layer_size = n;
+hidden_layer_size = 2;
+num_labels = size(y, 2);
 
 % Randomizing data 
 sel = randperm(m);
 X = X(sel, :);
 y = y(sel, :);
 
-initial_theta1 = randomInitializeWeights(input_layer_size, hidden_layer_size);
-initial_theta2 = randomInitializeWeights(hidden_layer_size, num_labels);
+e = 0.2;
+initial_theta1 = randomInitializeWeights(input_layer_size, hidden_layer_size, e);
+initial_theta2 = randomInitializeWeights(hidden_layer_size, num_labels, e);
+initial_omega = randomInitializeWeights(0, hidden_layer_size - 1, e);
 
-initial_nn_params = [initial_theta1(:); initial_theta2(:)];
+initial_nn_params = [initial_theta1(:); initial_theta2(:); initial_omega(:)];
 
 lambda = 0;
 
--- src/randomInitializeWeights.m
+++ src/randomInitializeWeights.m
@@ -1,6 +1,5 @@
-function [W] = randomInitializeWeights(in, out)
+function [W] = randomInitializeWeights(in, out, epsilon)
 %   epsilon is used so that the range of weights at the begining is going
 %   to be -epsilon till epsilon
-  epsilon = 0.2;
   W = rand(out, in+1) * 2 * epsilon - epsilon;
 end
